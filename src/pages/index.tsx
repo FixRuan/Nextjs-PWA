bef10be7e3b98b976646c9586bbabf9c412dfd7d
fixruan <ruangoio01@gmail.com>
2023-01-17 16:03:00 -0300

    test: render offline image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
 import React from "react";
+import logo from "../../public/logo.png";
 
 export default function Home() {
   return (
     <div>
       <h2>Hello PWA</h2>
+      <Image src={logo} alt="logo" width={100} />
     </div>
   );
 }
